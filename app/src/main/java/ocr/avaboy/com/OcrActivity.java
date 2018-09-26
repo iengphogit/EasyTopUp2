c26fd8c6f797b63689167f3ec984da31477f11cb
iengpho <ieng.pho@gaea-sys.com>
2018-09-26 23:28:46 +0700

    add touch attribute selectItem Background.

--- app/src/main/java/ocr/avaboy/com/OcrActivity.java
+++ app/src/main/java/ocr/avaboy/com/OcrActivity.java
@@ -6,16 +6,120 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 public class OcrActivity extends AppCompatActivity {
-
+    private SurfaceView mCameraView;
+    private CameraSource mCameraSource;
+    private TextView mTextView;
+    private static final String TAG = "OcrActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
+        mCameraView = findViewById(R.id.surfaceView);
+        mTextView = findViewById(R.id.text_view);
+        startCameraSource();
     }
 
 
 
+    private void startCameraSource() {
+
+        //Create the TextRecognizer
+        final TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (!textRecognizer.isOperational()) {
+            Toast.makeText(this, "Detector dependencies not loaded yet", Toast.LENGTH_SHORT).show();
+        } else {
+
+            //Initialize camerasource to use high resolution and set Autofocus on.
+            mCameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(true)
+                    .setRequestedFps(2.0f)
+                    .build();
+
+            /**
+             * Add call back to SurfaceView and check if camera permission is granted.
+             * If permission is granted we can start our cameraSource and pass it to surfaceView
+             */
+            mCameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    try {
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(OcrActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    202);
+                            return;
+                        }
+                        mCameraSource.start(mCameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                }
+
+                /**
+                 * Release resources for cameraSource
+                 */
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    mCameraSource.stop();
+                }
+            });
+
+            //Set the TextRecognizer's Processor.
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+                }
+
+                /**
+                 * Detect all the text from camera using TextBlock and the values into a stringBuilder
+                 * which will then be set to the textView.
+                 * */
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if (items.size() != 0 ){
+
+                        mTextView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i=0;i<items.size();i++){
+                                    TextBlock item = items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                mTextView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
 
 }
