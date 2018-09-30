f48eb87d31510c249eab972542dec5c18191de7a
iengpho <ieng.pho@gaea-sys.com>
2018-09-30 18:10:22 +0700

    add fragment layouts.

--- app/src/main/java/ocr/avaboy/com/MainActivity.java
+++ app/src/main/java/ocr/avaboy/com/MainActivity.java
@@ -8,44 +8,45 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import ocr.avaboy.com.fragment.BaseFragement;
+import ocr.avaboy.com.fragment.ServiceFragment;
+import ocr.avaboy.com.fragment.TopUpFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private final int REQUEST_CAMERA_CODE = 203;
     private ConstraintLayout rootView;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navi_menu);
 
-        if (getSupportActionBar() != null){
-            View actionBar = LayoutInflater.from(this).inflate(R.layout.custom_action_bar,null, false);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.menu_navi_top_up);
+
+        if (getSupportActionBar() != null) {
+            View actionBar = LayoutInflater.from(this).inflate(R.layout.custom_action_bar, null, false);
             getSupportActionBar().setCustomView(actionBar);
             getSupportActionBar().setDisplayShowCustomEnabled(true);
 
         }
-
-        rootView = findViewById(R.id.root_view);
-
-        View takeVideoMenu = findViewById(R.id.take_video);
-        takeVideoMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isHasCameraPermission()) {
-                    startOcrIntent();
-                } else {
-                    requestCamera();
-                }
-            }
-        });
     }
 
     private void startOcrIntent() {
@@ -91,4 +92,30 @@ public class MainActivity extends AppCompatActivity {
         snackbar.show();
     }
 
+    BaseFragement baseFragement;
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_navi_top_up:
+                baseFragement = new TopUpFragment();
+                setFragement(baseFragement);
+                return true;
+            case R.id.menu_navi_services:
+                baseFragement = new ServiceFragment();
+                setFragement(baseFragement);
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
+
+    private void setFragement(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame_view,fragment);
+        fragmentTransaction.commit();
+
+    }
 }
---
+++ app/src/main/java/ocr/avaboy/com/fragment/BaseFragement.java
@@ -0,0 +1,9 @@
+package ocr.avaboy.com.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by iengpho on 9/30/18.
+ */
+
+public class BaseFragement extends Fragment {}
